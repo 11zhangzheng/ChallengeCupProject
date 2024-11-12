1829552fdeb127a052fe724c83e74482f62044e8
798546123-beer <2561285872@qq.com>
2024-11-12 14:52:25 +0800

    新增traceInfo类

---
+++ springboot/src/main/java/com/cow/entity/TraceInfo.java
@@ -0,0 +1,35 @@
+package com.cow.entity;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "traceinfo")
+@NoArgsConstructor
+@AllArgsConstructor
+public class TraceInfo {
+    @Id
+    @TableId
+private String TraceCode;
+    @TableField("productName")
+    private String productName;
+    @TableField("productImage")
+    private String productImage;
+    @TableField("productDate")
+    private String productDate;
+    @TableField("specification")
+    private String specification;
+    @TableField("productionAddress")
+    private String productionAddress;
+    @TableField("manager")
+    private Integer managerId;
+
+}
