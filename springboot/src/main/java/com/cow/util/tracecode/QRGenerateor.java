1158444412659bc5e009e6b5badd4404b00cb013
798546123-beer <2561285872@qq.com>
2024-11-15 14:25:15 +0800

    完善后端生成QRCode的函数

--- springboot/src/main/java/com/cow/util/tracecode/QRGenerateor.java
+++ springboot/src/main/java/com/cow/util/tracecode/QRGenerateor.java
@@ -1,4 +1,21 @@
 package com.cow.util.tracecode;
 
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.extra.qrcode.QrCodeUtil;
+import cn.hutool.extra.qrcode.QrConfig;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
 public class QRGenerateor {
+    private static final String QR_PREFIX = "https://localhost:9999/"+"traceability?id=";
+    public static void generateQRCode(String content) {
+        try{
+            QrCodeUtil
+                    .generate(QR_PREFIX + content,(new QrConfig(320,320))
+                            .setErrorCorrection(ErrorCorrectionLevel.H),
+                            FileUtil.file("D:/QR/QRcode.jpg"));
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
+        }
+        return ;
+    }
 }
