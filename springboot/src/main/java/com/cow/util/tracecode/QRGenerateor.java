ec0c557b4f2fe9bb8d21c9d5d525c2d5ca755373
798546123-beer <2561285872@qq.com>
2024-11-16 08:07:44 +0800

    二维码生成函数加了测试类

--- springboot/src/main/java/com/cow/util/tracecode/QRGenerateor.java
+++ springboot/src/main/java/com/cow/util/tracecode/QRGenerateor.java
@@ -3,10 +3,14 @@ package com.cow.util.tracecode;
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.extra.qrcode.QrCodeUtil;
 import cn.hutool.extra.qrcode.QrConfig;
+import com.cow.util.general.PropertiesUtil;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
 public class QRGenerateor {
-    private static final String QR_PREFIX = "https://localhost:8080/"+"traceability?id=";
+    public static void main(String[] args){
+        generateQRCode("12345678900987654321");
+    }
+    private static final String QR_PREFIX = "http://"+PropertiesUtil.get+"/trace?id=";
     public static void generateQRCode(String content) {
         try{
             QrCodeUtil
