d2665a691a84ec97eb3c31c9c6d5d345b16cb3df
798546123-beer <2561285872@qq.com>
2024-11-17 19:56:59 +0800

    fix:修改允许跨域访问的源
    - 将允许跨域访问的源从 "*" 修改为 "http://localhost:8080/"
    - 这样可以提高系统安全性，避免潜在的安全风险

--- springboot/src/main/java/com/cow/config/WebConfig.java
+++ springboot/src/main/java/com/cow/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:8080/")
                 .allowedMethods("GET", "POST", "DELETE")
                 .allowedHeaders("*")
                 .exposedHeaders("Access-Control-Allow-Origin")
