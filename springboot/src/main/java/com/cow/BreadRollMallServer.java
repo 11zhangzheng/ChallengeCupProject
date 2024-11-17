bb32cf5d52d8ed8946191a3198aac67c0d75d704
798546123-beer <2561285872@qq.com>
2024-11-17 09:55:33 +0800

    一键启动

--- springboot/src/main/java/com/cow/BreadRollMallServer.java
+++ springboot/src/main/java/com/cow/BreadRollMallServer.java
@@ -1,9 +1,13 @@
 package com.cow;
 
+import com.cow.LangchainDemoApplication;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
@@ -12,7 +16,25 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @SpringBootApplication
 @MapperScan("com.cow.dao")
 public class BreadRollMallServer {
-    public static void main(String[] args) {
-        SpringApplication.run(BreadRollMallServer.class,args);
+    private static final Logger log = LoggerFactory.getLogger(BreadRollMallServer.class);
+
+        public static void test(String[] args) {
+        try {
+            startApplication(LangchainDemoApplication.class, "spring.jmx.default-domain=Mall", "com.sun.management.jmxremote.port=8082");
+            startApplication(BreadRollMallServer.class, "spring.jmx.default-domain=langchain", "com.sun.management.jmxremote.port=8003");
+        } catch (Exception e) {
+            log.error("Application startup failed", e);
+        }
+    }
+
+    private static void startApplication(Class<?> applicationClass, String... properties) {
+        log.info("Starting application: {}", applicationClass.getName());
+        new SpringApplicationBuilder(applicationClass)
+                .properties(properties)
+                .run();
+    }
+    public static void main(String[] args){
+        SpringApplication.run(LangchainDemoApplication.class, args);
     }
 }
+
